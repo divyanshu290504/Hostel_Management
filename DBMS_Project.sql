8257f84f162bc7d4ebe216e88b111c7a5a7e2901
Divyanshu Kumar <85216941+divyanshu290504@users.noreply.github.com>
2023-11-01 21:46:16 +0530

    Update DBMS_Project.sql

--- DBMS_Project.sql
+++ DBMS_Project.sql
@@ -2,13 +2,12 @@ drop database if exists hostel_db;
 create database hostel_db;
 use hostel_db;
 create table Hostelite(
-	Hostel_ID varchar(20) primary key,
+	Hostel_ID int primary key auto_increment, 
     Name varchar(20),
     Phone_No varchar(20),
     SRN varchar(20),
     DOB date,
     Legal_ID varchar(20),
-    Gender enum('Male','Female'),
     Hostel_Request_Status enum('0','1')
 );
 create table Local_Guardian(
@@ -41,7 +40,7 @@ create table Mess(
 	Mess_Name varchar(20),
     Month varchar(20),
     Year int,
-    Hostel_ID varchar(20) primary key,
+    Hostel_ID int primary key,
     foreign key(Hostel_ID) references Hostelite(Hostel_ID)
 );
 create table Leave_Request(
@@ -50,11 +49,11 @@ create table Leave_Request(
     Place varchar(20),
     Arrival_Datetime timestamp,
     Leaving_Datetime timestamp,
-    Hostel_ID varchar(20) primary key,
+    Hostel_ID int primary key,
     foreign key(Hostel_ID) references Hostelite(Hostel_ID)
 );
 create table Has_LG(
-	Hostel_ID varchar(20),
+	Hostel_ID int,
     Identification_No varchar(20),
     Relation varchar(20)
 );
@@ -62,27 +61,15 @@ alter table Has_LG add primary key(Hostel_ID,Identification_No);
 alter table Has_LG
 add foreign key(Hostel_ID) references Hostelite(Hostel_ID);
 create table Hostelite_Addr(
-	Hostelite_ID varchar(20),
-    Type enum('Temporary','Permanent'),
+	Hostelite_ID int,
     House_Details varchar(20),
     State varchar(20),
     City varchar(20),
     Pincode int,
     foreign key(Hostelite_ID) references Hostelite(Hostel_ID)
 );
-create table Parent_Guardian_Addr(
-	PID_No varchar(20),
-    Type enum('Temporary','Permanent'),
-    House_Details varchar(20),
-    State varchar(20),
-    City varchar(20),
-    Pincode int,
-    foreign key(PID_No) references Parent(Identification_No)
-);
-alter table Parent_Guardian_Addr add primary key(PID_No);
 create table Local_Guardian_Addr(
-	LG_ID varchar(20) primary key,
-    Type enum('Temporary','Permanent'),
+	LG_ID varchar(20),
     House_Details varchar(20),
     State varchar(20),
     City varchar(20),
@@ -90,7 +77,7 @@ create table Local_Guardian_Addr(
     foreign key(LG_ID) references Local_Guardian(Identification_No)
 );
 create table HasParent_Guardian(
-	Hostelite_ID varchar(20),
+	Hostelite_ID int,
     ID_Parent varchar(20),
     Relation varchar(20)
 );
